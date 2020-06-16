09cf6d5f6ed457214fb869d1ad8b2ca01de15b8b
sebasttti <juansjoya_r@hotmail.com>
2020-06-16 14:55:50 -0500

    libs agregados

--- app/libs/Controller.php
+++ app/libs/Controller.php
@@ -1,6 +1,37 @@
 <?php
 
+//se encarga de poder cargar los modelos y las vistas
 
+class Controller{
+    //cargar controlador
+    public function model($method,$subcontroller){
 
+        $method = ucfirst($method);
+        $subcontroller = ucfirst($subcontroller);
+       //
+      $string = '../app/models/'.$method.'/'.$subcontroller.'.php';
+    
+        if (file_exists($string)) {
+            require_once '../app/models/'.$method.'/'.$subcontroller.'.php';
+            //instanciar el modelo
+            return new $subcontroller;
+       }else{
+      //   //si el archivo no existe imprima un error
+         die('el modelo no existe');
+       }
+    }
 
-?>
+    //cargar vista
+    public function view($view,$data=[]){
+      //chequear si la vista existe
+      if (file_exists('../app/views/'.$view.'.php')) {
+          require_once('../app/views/'.$view.'.php');
+      }else{
+        //si el archivo no existe imprima un error
+        die('la vista no existe');
+      }
+    }
+
+}
+
+ ?>
--- app/libs/Core.php
+++ app/libs/Core.php
@@ -1,5 +1,73 @@
 <?php
 
+/*Mapear la URL ingresada en el navegador
+  1- controlador
+  2- metodo
+  3- parametro
+*/
 
+class Core{
+  protected $actualController = 'Index';
+  protected $actualMethod = 'main';
+  protected $parameters = [];
+
+
+  public function __construct(){
+    $url=$this->getUrl();
+
+        //primero reviso si la url[0] existe
+
+        if (isset($url[0])) {
+          //buscar en controladores si el controlador existe
+          if (file_exists('../app/controllers/'.ucwords($url[0]).'.php')) {
+              // setea el controlador actual
+              $this->actualController=ucwords($url[0]);
+              unset($url[0]);
+          }else{
+            // setea el controlador actual
+            $this->actualController="Error404";
+            unset($url[0]);
+          }
+        }
+
+
+          //aca trae la pagina
+          require_once('../app/controllers/'.$this->actualController.'.php');
+
+          //aca invoca la clase
+          //$this->actualController = new $this->actualController;
+
+        //chequear la segunda parte que seria el metodo
+        if (isset($url[1])) {
+          //if (method_exists($this->actualController,$url[1])) {
+              //chequeamos el metodo
+              $this->actualMethod = $url[1];
+              //desmontar el metodo
+              unset($url[1]);
+          //}
+        }
+        //chequear parametros
+
+        if ($url) {
+          foreach (array_values($url) as $key => $value) {
+            array_push($this->parameters,$value);
+          }
+        }
+
+        //hacer callback con parametros array
+        //echo $this->actualController;
+        $this->actualController = new $this->actualController($this->actualMethod,$this->parameters);
+  }
+
+  public function getUrl(){
+     if (isset($_GET['url'])) {
+          $url = rtrim($_GET['url'],'/');
+          $url = filter_var($url,FILTER_SANITIZE_URL);
+          $url = explode('/',$url);
+          return $url;
+      }
+  }
+
+}
 
 ?>
